67422c3e7cdd262936172b3564249de11b0c625c
tec1987 <tec163@163.com>
2020-03-13 19:37:57 +0800

    add default ports parameter %*

--- tcping/tt.cmd
+++ tcping/tt.cmd
@@ -1,9 +1,11 @@
-@echo off
+@echo off&SETLOCAL
+if #%1==# (set _pp=443) else set _pp=%*
+echo,**************** The default test Port is: %_pp%
 :bg
-SETLOCAL&set _port=443
 set /p ip="Please input IP address: "
-if "%ip%"=="cc" (cls&ENDLOCAL&goto :bg) else if "%ip%"=="q" ENDLOCAL&goto :eof
-for /f "eol=#tokens=1-2delims=:" %%a in ("%ip%") do set _ip=%%a&if not "%%b"=="" set _port=%%b
-if defined _ip tcping -i .3 -w 1 -j -f -n 6 -g 3 %_ip% %_port%
-echo,&ENDLOCAL
-goto :bg
+if "%ip%"=="cc" (cls&goto :bg) else if "%ip%"=="q" goto :eof
+for /f "eol=#tokens=1-2delims=:" %%a in ("%ip%") do set _ip=%%a&if "%%b"=="" (set _pt=) else set _pt=%%b
+if defined _pt (set _p=%_pt%) else set _p=%_pp%
+if defined _ip for %%a in (%_p%) do tcping -i .3 -w 1 -j -f -n 6 -g 3 %_ip% %%a
+echo,&goto :bg
+ENDLOCAL
